e11926f03e0ccfe8213660ce81db1433e64fd0fb
neflaabdul2004 <naflarv32@gmail.com>
2026-04-27 11:56:00 +0530

    program 14

---
+++ occurence.sh
@@ -0,0 +1,38 @@
+if [ $# -lt 2 ]
+then
+echo "Syntax is <$0> <word> <file....>"
+exit 1
+fi
+word=$1
+shift
+while [ $# -ne 0 ]
+do
+for wd in `cat $1`
+do
+if [ $wd = $word ]
+then
+count=`expr $count + 1`
+fi
+done
+shift
+done
+echo "Number of occurence of word =$count"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
